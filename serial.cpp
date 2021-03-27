ca0a950e17c5901ed8b14e131899ddc08fd6bbd8
Rushi Rajpara <Rushipatel11111@gmail.com>
2021-03-27 14:09:46 +0530

    minor changes

--- serial.cpp
+++ serial.cpp
@@ -1,55 +1,35 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-using namespace chrono;
-
-#define V 16
-#define INF 10000000
-
-int dist[V][V],i,j,k,l;
-const int RUNS = 100;
 
 int main()
 {
-    int graph[V][V] = { {0, 5, INF, 10, 0, 5, INF, 10, 0, 5, 1, 10, 0, 5, INF, 10},
-							{INF, 0, 3, INF, INF, 0, 3, INF, INF, 0, 3, INF, INF, 0, 3, INF},
-							{INF, INF, 0, 1, INF, INF, 0, 1, 7, INF, 0, 1, INF, INF, 0, 1},
-							{INF, 23, INF, 0, INF, INF, INF, 0, INF, INF, INF, 0, INF, INF, INF, 0},
-				{0, 5, INF, 10, 0, 5, INF, 10, 0, 5, INF, 10, 0, 5, 2, 10},
-							{INF, 0, 3, INF, 34, 0, 3, INF, INF, 0, 3, INF, INF, 0, 3, INF},
-							{INF, INF, 0, 1, INF, INF, 0, 1, INF, 89, 0, 1, INF, INF, 0, 1},
-							{INF, INF, INF, 0, INF, INF, INF, 0, INF, INF, INF, 0, INF, INF, INF, 0},
-				{0, 5, 2, 10, 0, 5, INF, 89, 0, 5, INF, 10, 0, 5, INF, 10},
-							{INF, 0, 3, INF, INF, 0, 3, INF, INF, 0, 3, INF, INF, 0, 3, INF},
-							{INF, INF, 0, 1, INF, INF, 0, 1, INF, INF, 0, 1, INF, INF, 0, 1},
-							{INF, INF, INF, 0, INF, 4, INF, 0, INF, INF, INF, 0, INF, INF, INF, 0},
-				{0, 5, INF, 10, 0, 5, INF, 10, 0, 5, INF, 10, 0, 5, INF, 10},
-							{INF, 0, 3, INF, INF, 0, 3, 8, INF, 0, 3, INF, INF, 0, 3, INF},
-							{INF, INF, 0, 1, INF, INF, 0, 1, INF, 7, 0, 1, INF, INF, 0, 1},
-							{INF, INF, INF, 0, INF, INF, INF, 0, INF, INF, 5, 0, INF, INF, INF, 0}
-						};
+    int n;
+    cin >> n;
 
-    for(int i = 0;i<V;i++)
-        for(int j = 0;j<V;j++)
-            dist[i][j] = graph[i][j];
+    vector<vector<int>> dist(n,vector<int>(n,0));
+    for(int i = 0;i<n;i++)
+        for(int j = 0;j<n;j++)
+            cin >> dist[i][j];
 
-    long long int sum = 0;
-
-    auto start = high_resolution_clock::now(); 
-    for (k = 0; k < V; k++)
+    for(int k = 0;k<n;k++)
     {
-        for (i = 0; i < V; i++)
+        for(int i = 0;i < n;i++)
         {
-            for (j = 0; j < V; j++)
+            for(int j = 0;j < n;j++)
             {
-                if (dist[i][k] + dist[k][j] < dist[i][j])
-                    dist[i][j] = dist[i][k] + dist[k][j];
+                dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j]);
             }
         }
     }
-    auto stop = high_resolution_clock::now(); 
-    double time_taken = duration_cast<chrono::nanoseconds>(stop - start).count(); 
 
-    cout << time_taken << endl; 
+    for(int i = 0;i<n;i++)
+    {
+        for(int j = 0;j<n;j++)
+            cout << dist[i][j] << " ";
+        cout << '\n';
+    }
+
+    return 0;
 
 }
