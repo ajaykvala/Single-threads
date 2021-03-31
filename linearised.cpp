d26e38d96487fad6e74c4c4e334ae6d9efa30db7
Rushi11111 <rushipatel11111@gmail.com>
2021-03-31 13:05:18 +0530

    omp_get_wtime

--- linearised.cpp
+++ linearised.cpp
@@ -4,46 +4,54 @@
 using namespace std;
 using namespace chrono;
 
-int** dist;
-int* linearRowWise;
+int dist[2048][2048];
+int linearRowWise[2048*2048];       //assign it manually or call malloc
+int n,x,i,j,k;
 
 int main()
 {
-    //statics
-
-    //variables
-    int n,x,i,j;
-
     //input
     fstream fin,fout;
-    string name = "./testcase/input/input42.txt";
+    string name = "./testcase/input/input02.txt";
     fin.open(name);
     fin >> n;
     
-    linearRowWise = (int*)malloc(n*n*sizeof(int));
-    dist = (int**)malloc(n*sizeof(int));
+    // linearRowWise = (int*)malloc(n*n*sizeof(int));
+    // dist = (int**)malloc(n*sizeof(int));
+    // for(i = 0;i<n;i++)
+    // {
+    //     dist[i] = (int*)malloc(n*sizeof(int));
+    // }
+
+    // for(i = 0;i<n;i++)
+    //     for(j = 0;j<n;j++)
+    //         cout << dist[i][j] << '\n';
+    // cout << "HI\n";
+
     for(i = 0;i<n;i++)
-        dist[i] = (int*)malloc(n*sizeof(int));
-    
-
-    for(int i = 0;i<n;i++)
-        for(int j = 0;j<n;j++)
+    {
+        for(j = 0;j<n;j++)
+        {
             fin >> dist[i][j];
+        }
+    }
 
-    for(int i = 0;i<n;i++)
-        for(int j = 0;j<n;j++)
+    for(i = 0;i<n;i++)
+        for(j = 0;j<n;j++)
         {
             linearRowWise[i + j*n] = dist[i][j];
         }
-    auto begin = std::chrono::high_resolution_clock::now();
-    for(int k = 0;k<n;k++)
+
+
+    double begin = omp_get_wtime();
+    for(k = 0;k<n;k++)
     {
         #pragma omp parallel for num_threads(4) private(i,j)
         for(i = 0;i < n;i++)
         {
             for(j = 0;j<n;j++)
             {
-                dist[i][j] = min(dist[i][j],linearRowWise[i + k*n] + dist[i][j]);
+                dist[i][j] = min(dist[i][j],linearRowWise[i + k*n] + dist[k][j]);
             }
         }
 
@@ -57,11 +65,11 @@ int main()
         }
     }
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
+    double end = omp_get_wtime();
+    double duration = end - begin;
 
     //validation
-    name = "./testcase/output/output42.txt";
+    name = "./testcase/output/output02.txt";
     fout.open(name);
     for(int i = 0;i<n;i++)
     {
@@ -72,12 +80,12 @@ int main()
             {
                 cout << i << " " << j << endl;
                 cout << x << " " << dist[i][j] << endl;
-                cout << "FAILED " << duration*1e-6 << "ms" << '\n';
+                cout << "FAILED " << duration*1000 << "ms" << '\n';
                 return 0;
             }
         }
     }
-    cout << "PASSED " << duration*1e-6 << "ms" << '\n';
+    cout << "PASSED " << duration*1000 << "ms" << '\n';
 
     fout.close();
     fin.close();
