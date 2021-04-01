e79c7d85c52656d394ab260b439b98a4b223ca72
Rushi11111 <rushipatel11111@gmail.com>
2021-04-01 14:39:05 +0530

    changed

--- testcase_generator.cpp
+++ testcase_generator.cpp
@@ -4,41 +4,56 @@ using namespace std;
 
 int main()
 {
-    string s = "./testcase/input/input";
-    string t = ".txt";
-    int ss = 0;             // starting number of testcase
-    int tt = 40;            // ending number of testcase
-    int intinf = 1e4;
-
-    std::mt19937_64 rng(time(0));
-    std::uniform_int_distribution<std::mt19937::result_type> dist(1,intinf);
-    std::uniform_int_distribution<std::mt19937::result_type> dist2(6,10);
-
-
-    for(int i1 = ss;i1<tt;i1++)
+    int size = 64;
+    int num = 7;
+    for(;size <= 4096;size *= 2)
     {
-        string nam;
-        nam += s;
-        nam += (char)(i1/10 + '0');
-        nam += (char)(i1%10 + '0');
-        nam += t;
-
-        fstream fin;
-        fin.open(nam,fstream::out);
-
-        int n = (1 << dist2(rng));
-        fin << n << '\n';
-
-        for(int i = 0;i<n;i++)
+        string s = "./testcase/";
+        int x = size;
+        string mp;
+        while(x > 0)
         {
-            for(int j = 0;j<n;j++)
-                fin << dist(rng) << " ";
-            fin << '\n';
+            mp += (char)(x%10 + '0');
+            x = x/10;
         }
+        reverse(mp.begin(),mp.end());
+        s += mp;
+        s += "/input";
+        string t = ".txt";
+        for(int yy = 0;yy < num;yy++)
+        {
+            cout << yy << '\n';
+            int intinf = 1e4;
 
-        fin.close();
-    } 
+            mt19937 rng(yy);
 
+            std::uniform_int_distribution<std::mt19937::result_type> dist(1,intinf);
+            std::uniform_int_distribution<std::mt19937::result_type> dist2(6,10);
+
+            string nam;
+            nam += s;
+            nam += (char)(yy/10 + '0');
+            nam += (char)(yy%10 + '0');
+            nam += t;
+
+            fstream fin;
+            cout << nam << endl;
+            fin.open(nam,fstream::out);
+
+            fin << size << '\n';
+
+            for(int i = 0;i<size;i++)
+            {
+                for(int j = 0;j<size;j++)
+                {
+                    fin << dist(rng) << " ";
+                }
+                fin << '\n';
+            }
+
+            fin.close();
+        } 
+    }
 
     return 0;
 }
